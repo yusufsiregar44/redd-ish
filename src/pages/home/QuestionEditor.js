5b67e90ea99106d460fc830007cae1b164abf767
yusufsiregar44 <yusufsiregar5@gmail.com>
2019-04-07 21:50:13 +0700

    Add code documentation

--- src/pages/home/QuestionEditor.js
+++ src/pages/home/QuestionEditor.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react'
+/**
+ * @todo
+ * 1) integrate onSubmit with redux action
+ */
+
+import React, { Component, StyleSheet } from 'react'
 import { TextInput, View, TouchableOpacity, Text } from 'react-native'
 
-import styles from './questionEditor/Style';
+// Styling
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    height: 50,
+    marginVertical: 5,
+  },
+  characterCounter: {
+    alignSelf: 'flex-end'
+  },
+  submitButton: {
+    alignSelf: 'center',
+    borderWidth: 1,
+    padding: 5
+  }
+})
 
 export default class QuestionEditor extends Component {
+  /**
+   * 1) text: state to contain string of question
+   */
   state = {
     text: ''
   }
 
+  /**
+   * updates the text state whenever there are changes in the text input
+   * @param text The newest value at TextInput
+   */
   _onChangeText = (text) => {
     this.setState({text})
   }
 
+  /**
+   * renders the remaining characters available for input. obtained by substracting the 
+   * state's text length with the integer 255
+   */
   _renderRemainingText = () => (
     <Text style={styles.characterCounter}>{this.state.text.length}/255</Text>
   )
 
+  /**
+   * submits the state's text through redux
+   */
   _onSubmit = () => {
     this.setState({text: ''})
   }
