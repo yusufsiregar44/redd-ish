896bf718602752247acf61e285a3ecaebef84fb2
yusufsiregar44 <yusufsiregar5@gmail.com>
2019-04-08 21:47:01 +0700

    feat: integrate redux and renders listOfQuestions from store

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native'
+import { connect } from 'react-redux';
 
-// Components
+// components
 import QuestionEditor from './home/QuestionEditor';
+import QuestionBox from './home/QuestionBox';
 
-// Styling
+// styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 10
   },
 })
-
-export default class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
         <QuestionEditor />
+        {
+          this.props.listOfquestions.map((question) => {
+            return (
+              <QuestionBox questionText={question.text} questionId={question.id} key={question.id} />
+            );
+          })
+        }
       </View>
     )
   }
 }
+
+// react-redux mapping to props
+const mapStateToProps = state => ({
+  listOfquestions: state.questions.listOfquestions
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+Home.propTypes = ({
+  
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
